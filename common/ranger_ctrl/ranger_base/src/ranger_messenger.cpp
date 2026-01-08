ffaed35114d5a3e147d52162f4eb88bc55510f01
JeongLi <lzh_jeong@qq.com>
2026-01-08 22:08:51 +0800

    disable print

--- common/ranger_ctrl/ranger_base/src/ranger_messenger.cpp
+++ common/ranger_ctrl/ranger_base/src/ranger_messenger.cpp
@@ -595,7 +595,7 @@ namespace westonrobot {
         l = robot_params_.wheelbase;
         w = robot_params_.track;
         phi_i = atan((l / 2) / (radius - w / 2));
-        ROS_INFO("command linear: %f, steering_angle: %f", linear, k * phi_i);
+        // ROS_INFO("command linear: %f, steering_angle: %f", linear, k * phi_i);
         return k * phi_i;
     }
 
